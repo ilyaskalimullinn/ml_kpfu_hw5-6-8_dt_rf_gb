63656e304757c4a9a42341eb7ab7c210894b176b
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-04-11 09:20:42 +0300

    [add] implement get_predictions [fix] some stuff

--- model/decision_tree.py
+++ model/decision_tree.py
@@ -15,14 +15,14 @@ class Node:
 
 class DT(ABC):
 
-    def __init__(self, max_depth, min_metric=0, min_elem=0):
+    def __init__(self, max_depth, min_metric=0, min_elem=1):
         self.max_depth = max_depth
         self.min_metric = min_metric
         self.min_elem = min_elem
         self.root = Node()
 
     def train(self, inputs, targets):
-        metric = self.__calc_metrics(targets)
+        metric = self._calc_metrics(targets)
         self.D = inputs.shape[1]
         self.__all_dim = np.arange(self.D)
 
@@ -40,7 +40,7 @@ class DT(ABC):
         return self.__all_dim
 
     @abstractmethod
-    def __create_term_value(self, targets: np.ndarray) -> Union[np.ndarray, float]:
+    def _create_term_value(self, targets: np.ndarray) -> Union[np.ndarray, float]:
         """
         :param targets: target values of train set that have reached this terminal node
         :return: terminal value (probability array for classification or a float for regression)
@@ -64,7 +64,7 @@ class DT(ABC):
 
     @staticmethod
     @abstractmethod
-    def __calc_metrics(targets: np.ndarray, *args, **kwargs) -> float:
+    def _calc_metrics(targets: np.ndarray, *args, **kwargs) -> float:
         pass
 
     def __inf_gain(self, targets_left: np.ndarray, targets_right: np.ndarray, parent_metric: Union[float, None] = None,
@@ -77,16 +77,18 @@ class DT(ABC):
         :return: information gain, metric for left node, metric for right node
         """
         if parent_metric is None:
-            parent_metric = self.__calc_metrics(np.vstack(targets_left, targets_right))
+            parent_metric = self._calc_metrics(np.hstack([targets_left, targets_right]))
         if N is None:
             N = targets_left.shape[0] + targets_right.shape[0]
 
-        metric_left = self.__calc_metrics(targets_left)
-        metric_right = self.__calc_metrics(targets_right)
+        metric_left = self._calc_metrics(targets_left)
+        metric_right = self._calc_metrics(targets_right)
 
         expected_metric = (targets_left.shape[0] / N) * metric_left + (targets_right.shape[0] / N) * metric_right
 
-        return parent_metric - expected_metric, metric_left, metric_right
+        inf_gain = parent_metric - expected_metric
+
+        return inf_gain, metric_left, metric_right
 
     def __build_splitting_node(self, inputs: np.ndarray, targets: np.ndarray, metric: float, N: Union[int, None] = None):
         """
@@ -115,13 +117,13 @@ class DT(ABC):
 
 
         for ax in self.__get_axis():
-            for th in self.__get_threshold(inputs[ax]):
-                idx_right = np.where(inputs[ax], inputs[ax] > th, True, False)
+            for th in self.__get_threshold(inputs[:, ax]):
+                idx_right = np.where(inputs[:, ax] > th, True, False)
                 idx_left = ~idx_right
 
-                metric_left, metric_right, information_gain = self.__inf_gain(targets[idx_left], targets[idx_right], metric)
+                information_gain, metric_left, metric_right = self.__inf_gain(targets[idx_left], targets[idx_right], N=N)
 
-                if information_gain > information_gain_max:
+                if information_gain >= information_gain_max:
                     ax_best = ax
                     th_best = th
                     information_gain_max = information_gain
@@ -130,38 +132,50 @@ class DT(ABC):
                     metric_left_best = metric_left
                     metric_right_best = metric_right
 
-        return ax_best, th_best, idx_right_best, idx_left_best, metric_left_best, metric_right_best,
+        return ax_best, th_best, idx_left_best, idx_right_best, metric_left_best, metric_right_best,
 
     def __build_tree(self, inputs, targets, node, depth, metric):
 
         N = len(targets)
         if depth >= self.max_depth or metric <= self.min_metric or N <= self.min_elem:
-            node.terminal_node = self.__create_term_value(targets)
+            node.terminal_node = self._create_term_value(targets)
         else:
 
             ax_max, th_max, ind_left_max, ind_right_max, disp_left_max, disp_right_max = self.__build_splitting_node(
                 inputs, targets, metric, N)
             node.split_ind = ax_max
             node.split_val = th_max
-            node.left = Node()
-            node.right = Node()
-            self.__build_tree(inputs[ind_left_max], targets[ind_left_max], node.left, depth + 1, disp_left_max)
-            self.__build_tree(inputs[ind_right_max], targets[ind_right_max], node.right, depth + 1, disp_right_max)
+            node.left_child = Node()
+            node.right_child = Node()
+            self.__build_tree(inputs[ind_left_max], targets[ind_left_max], node.left_child, depth + 1, disp_left_max)
+            self.__build_tree(inputs[ind_right_max], targets[ind_right_max], node.right_child, depth + 1, disp_right_max)
 
-    def get_predictions(self, inputs):
+    def get_predictions(self, inputs: np.ndarray) -> np.ndarray:
         """
         :param inputs: вектора характеристик
         :return: предсказания целевых значений
         """
-        pass
+        results = []
+        for input in inputs:
+            node = self.root
+
+            while node.terminal_node is None:
+                if input[node.split_ind] > node.split_val:
+                    node = node.right_child
+                else:
+                    node = node.left_child
+
+            results.append(node.terminal_node)
+
+        return np.vstack(results)
 
 
 class RegressionDT(DT):
     @staticmethod
-    def __calc_metrics(targets: np.ndarray, *args, **kwargs) -> float:
+    def _calc_metrics(targets: np.ndarray, *args, **kwargs) -> float:
         return RegressionDT.__variance(targets)
 
-    def __create_term_value(self, targets: np.ndarray) -> Union[np.ndarray, float]:
+    def _create_term_value(self, targets: np.ndarray) -> Union[np.ndarray, float]:
         return targets.mean()
 
     @staticmethod
@@ -175,18 +189,17 @@ class RegressionDT(DT):
 
 class ClassificationDT(DT):
 
-    def __init__(self, max_depth, number_classes, min_metric=0, min_elem=0):
+    def __init__(self, max_depth, number_classes, min_metric=0, min_elem=1):
         super().__init__(max_depth, min_metric, min_elem)
         self.K = number_classes
 
     @staticmethod
-    def __calc_metrics(targets: np.ndarray, *args, **kwargs) -> float:
+    def _calc_metrics(targets: np.ndarray, *args, **kwargs) -> float:
         return ClassificationDT.__shannon_entropy(targets)
 
-    def __create_term_value(self, targets: np.ndarray) -> np.ndarray:
-        y = np.arange(self.K)
-        y[targets] += 1
-        y = y / targets.shape[0]
+    def _create_term_value(self, targets: np.ndarray) -> np.ndarray:
+        y = np.bincount(targets, minlength=self.K)
+        y = y / len(targets)
         return y
 
     @staticmethod
@@ -197,6 +210,5 @@ class ClassificationDT(DT):
         """
         p = np.unique(targets, return_counts=True)[1]
         p = p / targets.shape[0]
-        print(p)
-        return -np.sum(p * np.log2(p))
-
+        res = -np.sum(p * np.log2(p))
+        return res
