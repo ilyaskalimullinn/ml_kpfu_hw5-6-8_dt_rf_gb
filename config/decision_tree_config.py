ffa67074173d2b786cbf4b201dd799fd8f9aa95c
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-04-22 17:22:05 +0300

    [edit] config, change preprocess to standardization

--- config/decision_tree_config.py
+++ config/decision_tree_config.py
@@ -9,6 +9,6 @@ cfg = EasyDict()
 # data
 cfg.train_set_percent = 0.8
 cfg.valid_set_percent = 0.1
-cfg.data_preprocess_type = DataProcessTypes.no_preprocess
+cfg.data_preprocess_type = DataProcessTypes.standardization
 
 cfg.wine_dataset_path = os.path.join(os.path.abspath(os.curdir), "static", "wine-quality-white-and-red.csv")
