313b88ad3e63a010cfcf2d11e16b29c23ce13d02
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-04-25 17:28:49 +0300

    [add] done bagging

--- main/main_random_forest.py
+++ main/main_random_forest.py
@@ -9,7 +9,85 @@ from model.random_forest import ClassificationRandomForest
 from utils.visualisation import Visualisation
 
 
+def visualize_experiments(experiments, best_experiments_amount, bagging=False):
+    training_type = "bagging" if bagging else "rno"
+
+    experiments.sort(key=lambda x: x["accuracy_valid"])
+
+    for experiment in experiments[:best_experiments_amount]:
+        model = experiment["model"]
+        experiment["accuracy_test"] = accuracy(
+            model.get_prediction(dataset.inputs_test, return_probability_vector=False), dataset.targets_test
+        )
+
+    visualisation = Visualisation(graphs_dir=GRAPHS_DIR)
+
+    visualisation.visualize_best_models(experiments[:best_experiments_amount],
+                                        title=f"Best {best_experiments_amount} models, {training_type}",
+                                        file_name=f"best_models_{training_type}.html", bagging=bagging)
+
+    best_model = experiments[0]["model"]
+    conf_matrix = confusion_matrix(best_model.get_prediction(dataset.inputs_test, return_probability_vector=False),
+                                   dataset.targets_test, dataset.k)
+
+    visualisation.visualize_confusion_matrix(conf_matrix, title=f"Confusion matrix, {training_type}",
+                                             file_name=f"confusion_matrix_{training_type}.html")
+
+
+def experiment_rno(dataset, models_amount, best_models_amount):
+    experiments = []
+
+    L_1_arr = np.random.randint(MIN_L1, MAX_L1, models_amount)
+    L_2_arr = np.random.randint(MIN_L2, MAX_L2, models_amount)
+    M_arr = np.random.randint(MIN_M, MAX_M, models_amount)
+    for L_1, L_2, M in zip(L_1_arr, L_2_arr, M_arr):
+        model = ClassificationRandomForest(M, MAX_DEPTH, MIN_ENTROPY, MIN_ELEMENTS_TERMINAL_NODE, CLASSES_AMOUNT, L_1,
+                                           L_2)
+
+        model.train(dataset.inputs_train, dataset.targets_train)
+
+        accuracy_valid = accuracy(model.get_prediction(dataset.inputs_valid, return_probability_vector=False),
+                                  dataset.targets_valid)
+
+        experiments.append({
+            "model": model,
+            "accuracy_valid": accuracy_valid,
+            "M": M,
+            "L_1": L_1,
+            "L_2": L_2
+        })
+
+    visualize_experiments(experiments, best_models_amount)
+
+
+def experiment_bagging(dataset, models_amount, best_models_amount):
+    experiments = []
+
+    M_arr = np.random.randint(MIN_M, MAX_M, models_amount)
+    bagging_percent_arr = np.random.uniform(size=(models_amount))
+
+    for M, bagging_percent in zip(M_arr, bagging_percent_arr):
+        model = ClassificationRandomForest(M, MAX_DEPTH, MIN_ENTROPY, MIN_ELEMENTS_TERMINAL_NODE,
+                                           CLASSES_AMOUNT, bagging_percent=bagging_percent)
+
+        model.train(dataset.inputs_train, dataset.targets_train)
+
+        accuracy_valid = accuracy(model.get_prediction(dataset.inputs_valid, return_probability_vector=False),
+                                  dataset.targets_valid)
+
+        experiments.append({
+            "model": model,
+            "accuracy_valid": accuracy_valid,
+            "M": M,
+            "bagging_percent": bagging_percent
+        })
+
+    visualize_experiments(experiments, best_models_amount, bagging=True)
+
+
 if __name__ == "__main__":
+    np.random.seed(2000)
+
     FOREST_AMOUNT = 30
     BEST_MODELS_AMOUNT = 10
     # L1 - max number of dimensions
@@ -33,42 +111,13 @@ if __name__ == "__main__":
 
     dataset = Digits(cfg)
 
-    experiments = []
+    training_type = input("Please, select training type: RNO or bagging: ").strip().lower()
 
-    L_1_arr = np.random.randint(MIN_L1, MAX_L1, FOREST_AMOUNT)
-    L_2_arr = np.random.randint(MIN_L2, MAX_L2, FOREST_AMOUNT)
-    M_arr = np.random.randint(MIN_M, MAX_M, FOREST_AMOUNT)
-    for L_1, L_2, M in zip(L_1_arr, L_2_arr, M_arr):
-        model = ClassificationRandomForest(M, MAX_DEPTH, MIN_ENTROPY, MIN_ELEMENTS_TERMINAL_NODE, L_1, L_2,
-                                           CLASSES_AMOUNT)
-
-        model.train(dataset.inputs_train, dataset.targets_train)
-
-        accuracy_valid = accuracy(model.get_prediction(dataset.inputs_valid, return_probability_vector=False),
-                                  dataset.targets_valid)
-
-        experiments.append({
-            "model": model,
-            "accuracy_valid": accuracy_valid,
-            "M": M,
-            "L_1": L_1,
-            "L_2": L_2
-        })
-
-    experiments.sort(key=lambda x: x["accuracy_valid"])
-
-    for experiment in experiments[:BEST_MODELS_AMOUNT]:
-        model = experiment["model"]
-        experiment["accuracy_test"] = accuracy(
-            model.get_prediction(dataset.inputs_test, return_probability_vector=False), dataset.targets_test
-        )
-
-    visualisation = Visualisation(graphs_dir=GRAPHS_DIR)
-
-    visualisation.visualize_best_models(experiments[:BEST_MODELS_AMOUNT], file_name="best_models.html")
-
-    best_model = experiments[0]["model"]
-    conf_matrix = confusion_matrix(best_model.get_prediction(dataset.inputs_test, return_probability_vector=False),
-                                   dataset.targets_test, dataset.k)
-
-    visualisation.visualize_confusion_matrix(conf_matrix, file_name="confusion_matrix.html")
+    if training_type == "rno":
+        print(f"Training {FOREST_AMOUNT} models using RNO...")
+        experiment_rno(dataset, FOREST_AMOUNT, BEST_MODELS_AMOUNT)
+    elif training_type == "bagging":
+        print(f"Training {FOREST_AMOUNT} models using bagging...")
+        experiment_bagging(dataset, FOREST_AMOUNT, BEST_MODELS_AMOUNT)
+    else:
+        print("Invalid training type")
--- utils/visualisation.py
+++ utils/visualisation.py
@@ -9,28 +9,47 @@ class Visualisation:
     def __init__(self, graphs_dir):
         self.graphs_dir = graphs_dir
 
-    def visualize_best_models(self, experiments, rounding=3, title='Лучшие модели Random Forest', file_name=None):
-        df = pd.DataFrame({
-            "L_1": [round(exp["L_1"], rounding) for exp in experiments],
-            "L_2": [round(exp["L_2"], rounding) for exp in experiments],
-            "M": [round(exp["M"], rounding) for exp in experiments],
-            "accuracy_valid": [round(exp["accuracy_valid"], rounding) for exp in experiments],
-            "accuracy_test": [round(exp["accuracy_test"], rounding) for exp in experiments]
-        })
+    def visualize_best_models(self, experiments, rounding=3, title='Лучшие модели Random Forest', file_name=None, bagging=False):
+        if bagging:
+            df = pd.DataFrame({
+                "accuracy_valid": [round(exp["accuracy_valid"], rounding) for exp in experiments],
+                "accuracy_test": [round(exp["accuracy_test"], rounding) for exp in experiments],
+                "M": [round(exp["M"], rounding) for exp in experiments],
+                "bagging_percent": [round(exp["bagging_percent"], rounding) for exp in experiments]
+            })
 
-        df["model_params"] = "L_1=" + df["L_1"].astype(str) + ", L_2=" + df["L_2"].astype(str) + ", M=" +\
-                             df["M"].astype(str)
+            df["model_params"] = "Bagging percent=" + df["bagging_percent"].astype(str) + ", M=" + df["M"].astype(str)
+
+            hover_data = {
+                "accuracy_valid": True,
+                "accuracy_test": True,
+                "M": True,
+                "bagging_percent": True,
+                "model_params": False
+            }
+        else:
+            df = pd.DataFrame({
+                "L_1": [round(exp["L_1"], rounding) for exp in experiments],
+                "L_2": [round(exp["L_2"], rounding) for exp in experiments],
+                "M": [round(exp["M"], rounding) for exp in experiments],
+                "accuracy_valid": [round(exp["accuracy_valid"], rounding) for exp in experiments],
+                "accuracy_test": [round(exp["accuracy_test"], rounding) for exp in experiments]
+            })
+            df["model_params"] = "L_1=" + df["L_1"].astype(str) + ", L_2=" + df["L_2"].astype(str) + ", M=" +\
+                                 df["M"].astype(str)
+            hover_data = {
+                'L_1': True,
+                'L_2': True,
+                'M': True,
+                "accuracy_valid": True,
+                "accuracy_test": True,
+                'model_params': False
+            }
 
         fig = px.line(data_frame=df,
                          x='model_params',
                          y='accuracy_valid',
-                         hover_data={'L_1': True,
-                                     'L_2': True,
-                                     'M': True,
-                                     "accuracy_valid": True,
-                                     "accuracy_test": True,
-                                     'model_params': False
-                                     })
+                         hover_data=hover_data)
 
         fig.update_layout(
             title=title,
