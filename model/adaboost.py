b7fc58b46fc6798f84930afe08b423237afe9a96
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-04-28 15:46:11 +0300

    [add] weights init

--- model/adaboost.py
+++ model/adaboost.py
@@ -2,12 +2,16 @@ import numpy as np
 
 
 class AdaBoost:
+
     def __init__(self, M):
         self.M = M
 
+        self.input_weights: np.ndarray
+        self.classifier_weights: np.ndarray
+
     def __init_weights(self, N):
         """ initialisation of input variables weights"""
-        pass
+        self.classifier_weights = np.ones(shape=(N,)) / N
 
     def update_weights(self, gt, predict, weights, weight_weak_classifiers):
         """ update weights functions DO NOT use loops"""
@@ -21,9 +25,8 @@ class AdaBoost:
         """ weak classifier weight calculation DO NOT use loops"""
         pass
 
-    def train(self, target, vectors):
+    def train(self, inputs, targets):
         """ train model"""
-        pass
 
     def get_prediction(self, vectors):
         """ adaboost get prediction """
